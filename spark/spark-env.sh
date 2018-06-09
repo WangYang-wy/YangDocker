2c82c5ae20ccf02a7996c478793a3b684309d5a6
WangYang-wy <1070942929@qq.com>
2018-06-09 23:14:37 +0800

    update: nothing important.

--- spark/spark-env.sh
+++ spark/spark-env.sh
@@ -1,7 +1,13 @@
 export SCALA_HOME=/usr/local/src/scala-2.12.4
+
 export JAVA_HOME=/usr/local/src/jdk1.8.0_152
-export HADOOP_HOME=/usr/local/src/hadoop-2.8.2
+
+export HADOOP_HOME=/usr/local/hadoop
+
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+
 SPARK_MASTER_IP=master
-SPARK_LOCAL_DIRS=/usr/local/src/spark-1.6.3-bin-hadoop2.6
+
+SPARK_LOCAL_DIRS=/usr/local/spark
+
 SPARK_DRIVER_MEMORY=1G
